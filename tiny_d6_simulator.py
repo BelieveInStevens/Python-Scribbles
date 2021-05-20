d6397c757eda0fde471e6ef1a8b5ebc22e63db1d
BelieveInStevens <steven.day.2010@gmail.com>
2021-05-19 23:01:33 -0600

    Added Learn Python The Hard Way Exercises

--- tiny_d6_simulator.py
+++ tiny_d6_simulator.py
@@ -2,39 +2,54 @@
 # tiny_d6_simulator: A program for testing the power of various things using the
 # Tiny D6 system.
 
-import random as r
+
+import matplotlib as plt
+import numpy as np
+import seaborn as sns
 from statistics import mean
 
-def tiny_d6_roll():
-    return r.randint(1,6)
+
+np.random.seed(777)
+n_sims = 100000
+cur_success = 5
+cur_crit = 5
+cur_state = 'Advantage'
+
+
+def d6_roll():
+    return np.random.randint(1,7)
 
 def tiny_d6_test(success_threshold, critical_threshold, advantage_state):
-    rolls = []
     if advantage_state == 'Advantage':
-        for i in range(3):
-            rolls.append(tiny_d6_roll())
+        rolls = 3
     if advantage_state == 'Disadvantage':
-        rolls.append(tiny_d6_roll())
+        rolls = 1
     else:
-        for i in range(2):
-            rolls.append(tiny_d6_roll())
-    if (advantage_state == 'Advantage' and 
-    len(rolls) == len(list(filter(lambda x: x >= critical_threshold, rolls)))):
-        return "Critical!"
-    elif len(list(filter(lambda x: x >= success_threshold, rolls))) >= 1:
+        rolls = 2
+    
+    results = []
+    for i in range(rolls):
+        results.append(d6_roll())
+
+    if (advantage_state == 'Advantage' and len(results) == 
+                            len(list(filter(lambda x: x >= critical_threshold, results)))):
+                                return "Critical!"
+    elif len(list(filter(lambda x: x >= success_threshold, results))) >= 1:
         return "Success!"
     else:
         return 'Failure!'
 
+
 def antimatter_missiles(success_threshold, critical_threshold, advantage_state):
     attack_roll = tiny_d6_test(success_threshold, critical_threshold, advantage_state)
-    damage = tiny_d6_roll()
+    damage = d6_roll()
     if attack_roll == 'Critical!':
-        return damage + 1
+        return damage * 2
     elif attack_roll == 'Success!':
         return damage
     else:
         return 0
+
 
 def armor_piercing_axe(success_threshold, critical_threshold, advantage_state):
     attack_rolls = []
@@ -42,6 +57,7 @@ def armor_piercing_axe(success_threshold, critical_threshold, advantage_state):
         if tiny_d6_test(success_threshold, critical_threshold, 'Disadvantage') == 'Success!':
             attack_rolls.append(1)
     return sum(attack_rolls)
+
 
 def plasma_cannons(success_threshold, critical_threshold, advantage_state):
     attack_roll1 = tiny_d6_test(success_threshold, critical_threshold, advantage_state)
@@ -61,12 +77,14 @@ def plasma_cannons(success_threshold, critical_threshold, advantage_state):
     else:
         return attack_damage1 + attack_damage2 
 
+
 def charge_cannon(success_threshold, critical_threshold, advantage_state):
     attack_roll = tiny_d6_test(success_threshold, critical_threshold, 'Disadvantage')
     if attack_roll == 'Success!':
         return 6
     else:
         return 0
+
 
 def rapid_fire_lasers(success_threshold, critical_threshold, advantage_state):
     num_tests = 3
@@ -79,24 +97,27 @@ def rapid_fire_lasers(success_threshold, critical_threshold, advantage_state):
             num_tests -= 1
     return damage
 
+
 def big_glowy_sword(success_threshold, critical_threshold, advantage_state):
     attack_roll = tiny_d6_test(success_threshold, critical_threshold, advantage_state)
     if attack_roll == 'Critical!':
-        return 4
+        return 6
     elif attack_roll == 'Success!':
         return 3
     else:
         return 0
-    
+
+
 def main_cannon(success_threshold, critical_threshold, advantage_state):
     attack_roll = tiny_d6_test(success_threshold, critical_threshold, advantage_state)
     if attack_roll == 'Critical':
-        damage = 3
+        damage = 4
     elif attack_roll == 'Success!':
         damage = 2
     else: 
         damage = 0
     return damage
+
 
 def automatic_cannon(success_threshold, critical_threshold, advantage_state):
     attack_roll1 = tiny_d6_test(success_threshold, critical_threshold, advantage_state)
@@ -115,15 +136,17 @@ def automatic_cannon(success_threshold, critical_threshold, advantage_state):
         attack2_damage = 0
     return attack1_damage + attack2_damage
 
+
 def retractable_sword(success_threshold, critical_threshold, advantage_state):
     attack_roll = tiny_d6_test(success_threshold, critical_threshold, advantage_state)
     if attack_roll == 'Critical!':
-        damage = 3
+        damage = 4
     elif attack_roll == 'Success!':
         damage = 2
     else:
         damage = 0
     return damage
+
 
 def swarm_missiles(success_threshold, critical_threshold, advantage_state):
     num_tests = 6
@@ -138,8 +161,27 @@ def swarm_missiles(success_threshold, critical_threshold, advantage_state):
     else:
         return damage
 
+
+record_antimatter_missiles = []
+record_swarm_missiles = []
+record_rapid_fire_lasers = []
+
+for runs in range(n_sims):
+    record_antimatter_missiles.append(antimatter_missiles(cur_success, cur_crit, cur_state))
+    record_swarm_missiles.append(antimatter_missiles(cur_success, cur_crit, cur_state))
+    record_rapid_fire_lasers.append(antimatter_missiles(cur_success, cur_crit, cur_state))
+
 def tiny_d6_simulate(weapon, success_threshold, critical_threshold, advantage_state):
     results = []
     for x in range(10000):
         results.append(weapon(success_threshold, critical_threshold, advantage_state))
     return mean(results)
+
+ax = sns.distplot(record_rapid_fire_lasers)
+# plt.title("Histogram of Weapon Damage")
+ax.set_xlabel("Damage")
+ax.set_ylabel("Count")
+
+sns.distplot(record_antimatter_missiles)
+sns.distplot(record_rapid_fire_lasers)
+sns.distplot(record_swarm_missiles)
